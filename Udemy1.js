8df7abd0d9d6922e6d58f8a47a743503f29b1e92
kolesnikaleks <kolesnikaleksv@gmail.com>
2019-11-22 15:33:52 +0200

    last chenge

--- Udemy1.js
+++ Udemy1.js
@@ -1,24 +1,28 @@
 
+"use strict";
+
 let money = prompt("Ваш бюджет на месяц?", "укажите в гривнах"),
     time = prompt("Введите дату в формате YYYY-MM-DD", "год, месяц, день");
 
 let appData = {
     budget: money,
     timeDate: time,
-    expenses: {},
+    expenses: {
+        a1: a3
+    },
     optionalExpenses: {},
     income: [],
     saving: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце",
-     "Укажите в гривнах"),
+var a1 = prompt("Введите обязательную статью расходов в этом месяце",""),
     a2 = prompt("Во сколько обойдется", "Укажите в гривнах"),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", 
-     "Укажите в гривнах"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
     a4 = prompt("Во сколько обойдется", "Укажите в гривнах");
 
 appData.expenses.a1 = a2;
 appData.expenses.a3 = a4;
 
 alert(appData.budget / 30);
+
+console.log(appData.expenses);
