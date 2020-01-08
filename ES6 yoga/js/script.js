8ebee0c4c0e5bb732f149960d67d590897b7ebb7
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-01-08 15:36:53 +0200

    ES6

---
+++ ES6 yoga/js/script.js
@@ -0,0 +1,114 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+"use strict";
+let tab = document.querySelectorAll(".info-header-tab"),
+    info = document.querySelector(".info-header"),
+    tabContent = document.querySelectorAll(".info-tabcontent");
+
+
+   let hideTabContent = (a) => {
+       for(let i = a; i < tabContent.length; i++) {
+           tabContent[i].classList.remove("show");
+           tabContent[i].classList.add("hide");
+       }
+   }
+   hideTabContent(1);
+
+   let showTabContent = (b) => {
+      
+     if(tabContent[b].classList.contains("hide")) {
+        tabContent[b].classList.remove("hide");
+        tabContent[b].classList.add("show");
+        }
+    }
+   
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if(target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+    // timer
+let deadLine = "2020-1-10";
+
+let getTimeRemaining = (endtime) => {
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    
+    let seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor(t/(1000*60*60));
+        
+    return {
+        "total" : t,
+        "seconds" : seconds,
+        "minutes" : minutes,
+        "hours" : hours
+    };
+}
+let setClock = (id, endtime) => {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector(".hours"),
+        minutes = timer.querySelector(".minutes"),
+        seconds = timer.querySelector(".seconds"),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+        let addZero = (num) => {
+            if(num <= 9) {
+                return "0" + num;
+            } else return num;
+        };
+        
+        seconds.textContent = t.seconds;
+        minutes.textContent = t.minutes;
+        hours.textContent = t.hours;
+    if(t.total <= 0) {
+        clearInterval(timeInterval);
+        seconds.textContent = "00";
+        minutes.textContent = "00";
+        hours.textContent = "00";
+    }
+    }
+};
+setClock("timer", deadLine);
+
+
+// Modal window
+let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close"),
+    inf = document.querySelector(".info");
+    
+
+
+inf.addEventListener("click", function() {
+    if(event.target && event.target.classList.contains("description-btn")) { 
+        overlay.style.display = "block";
+         this.classList.add("more-splash");
+         document.body.style.overflow = "hidden";
+    }
+});
+
+more.addEventListener("click", function () {
+    overlay.style.display = "block";
+    this.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+});
+close.addEventListener("click", function() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+});
+
+
+
+
+
+});
---
+++ ES6 yoga/task_two/options.js
@@ -0,0 +1,55 @@
+
+
+// let age = document.getElementById('age');
+  
+// function showUser(surname, name) {
+//     this.value = age.value;
+// 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+
+// showUser("kolesnik", "alex");
+
+class Options {
+    constructor(height, width, bg, fontSize, textAlign, text) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+        this.text = text;
+        this.createElem = function() {
+        let div = document.createElement("div");
+        div.classList.add("box");
+        document.body.appendChild(div);
+        div.style.height = this.height + "px";
+        div.style.width = this.width + "px";
+        div.style.backgroundColor = this.bg;
+        div.style.fontSize = this.fontSize + "px";
+        div.style.textAlign = this.textAlign;
+        div.textContent = this.text;
+        
+        
+         }
+         
+}   
+};
+
+let divElem = new Options("100","200","red","35", "center", "Yes"),
+    newDiv = new Options("200", "500", "blue", "20", "end", "Привет как дела?");
+    newD = new Options("100", "300", "yellow", "30", "left", "Я супер крутой програмист!");
+
+console.log(divElem.createElem());
+console.log(newDiv.createElem());
+console.log(newD.createElem());
+// let div = document.createElement("div");
+//     div.classList.add("box");
+//     document.body.appendChild(div);
+//     div.style.backgroundColor = "red";
+//     div.style.height = "100px";
+//     div.style.width = "100px";
+//     div.style.fontSize = "20px";
+//     div.style.textContent = "Hello world!";
+//     div.innerHTML = "<h1>Hello world</h1>";
+    
+// console.log(div);
+
